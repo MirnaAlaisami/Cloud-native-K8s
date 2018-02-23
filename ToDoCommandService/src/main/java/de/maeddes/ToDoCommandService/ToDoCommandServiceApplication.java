c4079d4a908ee226afdeee51e5687e73e7906ca4
Alaisami Mirna <Mirna.Alaisami@novatec-gmbh.de>
2018-02-23 16:58:43 +0100

    K8s-code-v2

--- ToDoCommandService/src/main/java/de/maeddes/ToDoCommandService/ToDoCommandServiceApplication.java
+++ ToDoCommandService/src/main/java/de/maeddes/ToDoCommandService/ToDoCommandServiceApplication.java
@@ -40,7 +40,7 @@ public class ToDoCommandServiceApplication {
     private Queue queue;
 
     public void send(String message) {
-        //this.rabbitTemplate.convertAndSend(queue.getName(), message);
+        this.rabbitTemplate.convertAndSend(queue.getName(), message);
         System.out.println(" [x] Sent '" + message + "'");
     }
 
